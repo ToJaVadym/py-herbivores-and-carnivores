e66067d3c0594f7325d03281598036829711dd0c
ToJaVadym <vadymivaneiko2@gmail.com>
2026-04-02 13:37:10 +0200

    Code style changed

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
 class Animal:
     alive: list["Animal"] = []
-    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
-        self.health = health
+
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
         self.name = name
+        self.health = health
         self.hidden = hidden
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
         return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+
 
 class Herbivore(Animal):
     def hide(self) -> None:
@@ -18,6 +25,7 @@ class Carnivore(Animal):
     def bite(self, target: Animal) -> None:
         if isinstance(target, Herbivore) and not target.hidden:
             target.health -= 50
+
             if target.health <= 0:
                 target.health = 0
                 if target in Animal.alive:
